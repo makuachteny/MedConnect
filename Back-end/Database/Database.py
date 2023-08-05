48c0950b0b54e331abea8167c3fbfe8d7910aa1c
mnshimiyimana <m.nshimiyim@alustudent.com>
2023-08-05 10:15:41 +0200

    updates

--- Back-end/Database/Database.py
+++ Back-end/Database/Database.py
@@ -31,6 +31,33 @@ patients_data = [
 insert_patient_query = "INSERT INTO Patients (ID, Patient_Name, DateOfBirth, Gender, Phone_Number, DoctorID) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
 cursor.executemany(insert_patient_query, patients_data)
 
+# Insert data into the Appointments table
+appointments_data = [
+    ("1001", "2021-05-15", "10:00:00", "1001", "1001"),
+    ("1002", "2021-05-15", "11:00:00", "1002", "1002"),
+    ("1003", "2021-05-15", "12:00:00", "1003", "1003"),
+]
+
+insert_appointment_query = "INSERT INTO Appointments (ID, Date, Time, PatientID, DoctorID) VALUES (%s, %s, %s, %s, %s)"
+cursor.executemany(insert_appointment_query, appointments_data)
+
+# Insert data into the hospitals table
+hospitals_data = [
+    ("1001", "Kanombe Hospital", "123 Main St City", "555-123-4567"),
+    ("1002", "Plateau Poly-Clinic", "456 Main St City", "555-987-6543"),
+    ("1003", "King Faisal Hospital", "789 Main St City", "555-555-5555"),
+    ("1004", "CHUK", "789 Main St City", "555-555-5555")
+    ("1005", "CHUB", "789 Main St City", "555-555-5555")
+    ("1006", "PolyFam Poly-Clinic", "789 Main St City", "555-555-5555")
+]
+
+insert_hospital_query = "INSERT INTO Hospitals (ID, Hospital_Name, Address, Phone_Number) VALUES (%s, %s, %s, %s)"
+cursor.executemany(insert_hospital_query, hospitals_data)
+
+# Insert data into the Patient reports table
+
+
+
 # Commit changes and close the cursor and connection
 db.commit()
 cursor.close()
