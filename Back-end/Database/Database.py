c82da1eb03420c1704360bf9616e6fbd4963e5f5
makuachteny <m.gatluak@alustudent.com>
2023-08-07 02:13:02 +0300

    update database

--- Back-end/Database/Database.py
+++ Back-end/Database/Database.py
@@ -11,6 +11,53 @@ db = mysql.connector.connect(
 # Create a cursor object to interact with the database
 cursor = db.cursor()
 
+# Create Doctors table
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Doctors (
+        ID INT AUTO_INCREMENT PRIMARY KEY,
+        DoctorName VARCHAR(255) NOT NULL,
+        Address VARCHAR(255),
+        Phone_Number VARCHAR(20),
+        Specialty VARCHAR(255),
+        Hospital VARCHAR(255)
+    )
+""")
+
+# Create Patients table
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Patients (
+        ID INT AUTO_INCREMENT PRIMARY KEY,
+        Patient_Name VARCHAR(255) NOT NULL,
+        DateOfBirth DATE,
+        Gender ENUM('Male', 'Female', 'Other'),
+        Phone_Number VARCHAR(20),
+        DoctorID INT,
+        FOREIGN KEY (DoctorID) REFERENCES Doctors(ID)
+    )
+""")
+
+# Create Appointments table
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Appointments (
+        ID INT AUTO_INCREMENT PRIMARY KEY,
+        Date DATE,
+        Time TIME,
+        PatientID INT,
+        DoctorID INT,
+        FOREIGN KEY (PatientID) REFERENCES Patients(ID),
+        FOREIGN KEY (DoctorID) REFERENCES Doctors(ID)
+    )
+""")
+
+# Create Hospitals table
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Hospitals (
+        ID INT AUTO_INCREMENT PRIMARY KEY,
+        Hospital_Name VARCHAR(255) NOT NULL,
+        Address VARCHAR(255),
+        Phone_Number VARCHAR(20)
+    )
+""")
 # Insert data into the Doctors table
 doctors_data = [
     ("1001", "Dr. Smith", "123 Main St City", "555-123-4567", "Cardiology", "General Hospital"),
@@ -18,7 +65,7 @@ doctors_data = [
     ("1003", "Dr. Davis", "789 Main St City", "555-555-5555", "Dermatology", "Skin & Wellness Center"),
 ]
 
-insert_doctor_query = "INSERT INTO Doctors (Id, DoctorName, Address, Phone_Number, Specialty, Hospital) VALUES (%s, %s, %s, %s, %s, %s)"
+insert_doctor_query = "INSERT INTO Doctors (ID, DoctorName, Address, Phone_Number, Specialty, Hospital) VALUES (%s, %s, %s, %s, %s, %s)"
 cursor.executemany(insert_doctor_query, doctors_data)
 
 # Insert data into the Patients table
@@ -54,8 +101,25 @@ hospitals_data = [
 insert_hospital_query = "INSERT INTO Hospitals (ID, Hospital_Name, Address, Phone_Number) VALUES (%s, %s, %s, %s)"
 cursor.executemany(insert_hospital_query, hospitals_data)
 
-# Insert data into the Patient reports table
+# Create PatientsReports table
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS PatientsReports (
+        ID INT AUTO_INCREMENT PRIMARY KEY,
+        PatientID INT,
+        Report TEXT,
+        ReportDate DATE,
+        FOREIGN KEY (PatientID) REFERENCES Patients(ID)
+    )
+""")
 
+# Insert data into the PatientsReports table
+reports_data = [
+    ("1", "Patient 1's report", "2021-05-15"),
+    ("2", "Patient 2's report", "2021-05-16"),
+    ("3", "Patient 3's report", "2021-05-17"),
+]
+insert_report_query = "INSERT INTO PatientsReports (PatientID, Report, ReportDate) VALUES (%s, %s, %s)"
+cursor.executemany(insert_report_query, reports_data)
 
 
 # Commit changes and close the cursor and connection
