ec6eec52ba0c43ad2426af29b1aa781b1e1d481e
makuachteny <m.gatluak@alustudent.com>
2023-08-08 15:08:18 +0300

    Database

--- Back-end/Database/Database.py
+++ Back-end/Database/Database.py
@@ -2,10 +2,10 @@ import mysql.connector
 
 # Create a connection to the MySQL database
 db = mysql.connector.connect(
-    host="Localhost",
-    user="",
-    password="",
-    database="MedConnect"
+    host="localhost",
+    user="teny229",
+    password="password",
+    database="hospital_records"
 )
 
 # Create a cursor object to interact with the database
@@ -58,48 +58,6 @@ cursor.execute("""
         Phone_Number VARCHAR(20)
     )
 """)
-# Insert data into the Doctors table
-doctors_data = [
-    ("1001", "Dr. Smith", "123 Main St City", "555-123-4567", "Cardiology", "General Hospital"),
-    ("1002", "Dr. Johnson", "456 Main St City", "555-987-6543", "Pediatrics", "Children's Clinic"),
-    ("1003", "Dr. Davis", "789 Main St City", "555-555-5555", "Dermatology", "Skin & Wellness Center"),
-]
-
-insert_doctor_query = "INSERT INTO Doctors (ID, DoctorName, Address, Phone_Number, Specialty, Hospital) VALUES (%s, %s, %s, %s, %s, %s)"
-cursor.executemany(insert_doctor_query, doctors_data)
-
-# Insert data into the Patients table
-patients_data = [
-    ("1001", "John Doe", "1990-05-15", "Male", "123-456-7890", "1"),
-    ("1002", "Jane Smith", "1985-08-20", "Female", "987-654-3210", "2"),
-    ("1003", "Michael Johnson", "1978-03-10", "Male", "555-123-4567", "3"),
-]
-
-insert_patient_query = "INSERT INTO Patients (ID, Patient_Name, DateOfBirth, Gender, Phone_Number, DoctorID) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-cursor.executemany(insert_patient_query, patients_data)
-
-# Insert data into the Appointments table
-appointments_data = [
-    ("1001", "2021-05-15", "10:00:00", "23456778", "1001"),
-    ("1002", "2021-05-15", "11:00:00", "2334567778", "1002"),
-    ("1003", "2021-05-15", "12:00:00", "8996242526", "1003"),
-]
-
-insert_appointment_query = "INSERT INTO Appointments (ID, Date, Time, PatientID, DoctorID) VALUES (%s, %s, %s, %s, %s)"
-cursor.executemany(insert_appointment_query, appointments_data)
-
-# Insert data into the hospitals table
-hospitals_data = [
-    ("1001", "Kanombe Hospital", "123 Main St City", "555-123-4567"),
-    ("1002", "Plateau Poly-Clinic", "456 Main St City", "555-987-6543"),
-    ("1003", "King Faisal Hospital", "789 Main St City", "555-555-5555"),
-    ("1004", "CHUK", "789 Main St City", "555-555-5555")
-    ("1005", "CHUB", "789 Main St City", "555-555-5555")
-    ("1006", "PolyFam Poly-Clinic", "789 Main St City", "555-555-5555")
-]
-
-insert_hospital_query = "INSERT INTO Hospitals (ID, Hospital_Name, Address, Phone_Number) VALUES (%s, %s, %s, %s)"
-cursor.executemany(insert_hospital_query, hospitals_data)
 
 # Create PatientsReports table
 cursor.execute("""
@@ -112,15 +70,58 @@ cursor.execute("""
     )
 """)
 
-# Insert data into the PatientsReports table
-reports_data = [
-    ("1", "Patient 1's report", "2021-05-15"),
-    ("2", "Patient 2's report", "2021-05-16"),
-    ("3", "Patient 3's report", "2021-05-17"),
+# Insert data into the Doctors table
+doctors_data = [
+    ("Dr. Smith", "123 Main St City", "555-123-4567",
+     "Cardiology", "General Hospital"),
+    ("Dr. Johnson", "456 Main St City", "555-987-6543",
+     "Pediatrics", "Children's Clinic"),
+    ("Dr. Davis", "789 Main St City", "555-555-5555",
+     "Dermatology", "Skin & Wellness Center"),
 ]
-insert_report_query = "INSERT INTO PatientsReports (PatientID, Report, ReportDate) VALUES (%s, %s, %s)"
-cursor.executemany(insert_report_query, reports_data)
 
+insert_doctor_query = "INSERT INTO Doctors (DoctorName, Address, Phone_Number, Specialty, Hospital) VALUES (%s, %s, %s, %s, %s)"
+cursor.executemany(insert_doctor_query, doctors_data)
+
+# Insert data into the Patients table
+patients_data = [
+    ("John Doe", "1990-05-15", "Male", "123-456-7890", 1),
+    ("Jane Smith", "1985-08-20", "Female", "987-654-3210", 2),
+    ("Michael Johnson", "1978-03-10", "Male", "555-123-4567", 3),
+]
+
+insert_patient_query = "INSERT INTO Patients (Patient_Name, DateOfBirth, Gender, Phone_Number, DoctorID) VALUES (%s, %s, %s, %s, %s)"
+cursor.executemany(insert_patient_query, patients_data)
+
+# Insert data into the Appointments table
+appointments_data = [
+    ("2021-05-15", "10:00:00", 1, 1),
+    ("2021-05-15", "11:00:00", 2, 2),
+    ("2021-05-15", "12:00:00", 3, 3),
+]
+
+insert_appointment_query = "INSERT INTO Appointments (Date, Time, PatientID, DoctorID) VALUES (%s, %s, %s, %s)"
+cursor.executemany(insert_appointment_query, appointments_data)
+
+# Insert data into the Hospitals table
+hospitals_data = [
+    ("Kanombe Hospital", "123 Main St City", "555-123-4567"),
+    ("Plateau Poly-Clinic", "456 Main St City", "555-987-6543"),
+    ("King Faisal Hospital", "789 Main St City", "555-555-5555"),
+]
+
+insert_hospital_query = "INSERT INTO Hospitals (Hospital_Name, Address, Phone_Number) VALUES (%s, %s, %s)"
+cursor.executemany(insert_hospital_query, hospitals_data)
+
+# Insert data into the PatientsReports table
+medical_record_data = [
+    (1, "Patient 1's report", "2021-05-15"),
+    (2, "Patient 2's report", "2021-05-16"),
+    (3, "Patient 3's report", "2021-05-17"),
+]
+
+insert_medical_record_query = "INSERT INTO PatientsReports (PatientID, Report, ReportDate) VALUES (%s, %s, %s)"
+cursor.executemany(insert_medical_record_query, reports_data)
 
 # Commit changes and close the cursor and connection
 db.commit()
