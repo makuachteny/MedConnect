db9b10af542072aedb1bd4d1b03317ec230a121a
makuachteny <m.gatluak@alustudent.com>
2023-08-09 16:08:23 +0300

    update on databases

--- CREATE_USER.sql
+++ CREATE_USER.sql
@@ -1,6 +1,6 @@
 -- Create the new user
 CREATE USER 'admin' @'localhost' IDENTIFIED WITH mysql_native_password BY 'Medconnect!';
 -- Grant all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'admin23' @'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'admin' @'localhost';
 -- Apply changes
 FLUSH PRIVILEGES;
