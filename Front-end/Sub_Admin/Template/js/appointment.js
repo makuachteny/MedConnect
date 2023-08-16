883e0b2678cd57bc01b1996a07cba6c9458cb8d8
rumunyana <113432535+rumunyana@users.noreply.github.com>
2023-08-16 19:44:46 +0200

    changes

---
+++ Front-end/Sub_Admin/Template/js/appointment.js
@@ -0,0 +1,134 @@
+const AppointmentId = localStorage.getItem('AppointmentId')
+const baseUrl = "http://localhost:7001/"
+
+async function createAppointment(e) {
+    if(e){
+      e.preventDefault();
+    }
+    if(AppointmentId){
+      await fetch(baseUrl + `api/v1/appointment/${AppointmentId}`, {
+        method: 'PATCH',
+        headers:{
+          Accept: "application/json",
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        Appointment_Date : document.getElementById('Appointment_Date').value,
+        Appointment_Time : document.getElementById('Appointment_Time').value,
+        Patient_ID : document.getElementById("Patient_ID").value,
+      })
+    });
+    localStorage.removeItem("AppointmentId");
+
+    }else{
+      const res = await fetch(baseUrl+"api/v1/appointment",{
+        method: "POST",
+        headers:{
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          Appointment_Date : document.getElementById('Appointment_Date').value,
+          Appointment_Time : document.getElementById('Appointment_Time').value,
+          Patient_ID : document.getElementById("Patient_ID").value,
+        })
+    })
+    }
+    document.getElementById('Appointment-form').reset();
+    
+  }
+  
+  
+  
+  const appointBtn = document.getElementById('submit-appointment');
+  appointBtn.addEventListener('click',createAppointment)
+    
+  
+  
+  async function retrieveAppointments(){
+    fetch(baseUrl + 'api/v1/Appointment')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      let containerDiv = document.querySelector('.appointment-records');
+      for (let i = 0; i < data.data.length; i++) {
+        let html = `<tr data-AppointmentId="${data.data[i].AppointmentId}">
+                      <td>${data.data[i].AppointmentId}</td>
+                      <td>${data.data[i].Patient_ID}</td>
+                      <td>${data.data[i].Appointment_Date}</td>
+                      <td>${data.data[i].Appointment_Time}</td>
+                      <td>
+                        <i class="fas fa-edit action-btn update-btn"></i>
+                        <i class="fas fa-trash-alt action-btn delete-btn"></i>
+                      </td>
+                    </tr>`;
+        containerDiv.insertAdjacentHTML('beforeend', html);
+      }
+      const tableEl = document.querySelector('.tableEl');
+      tableEl.addEventListener('click',deleteAppointmentInfo);
+      const updateEl = document.querySelector('.tableEl')
+    updateEl.addEventListener('click',UpdateAppointmentInfo);
+    });
+  }
+
+  function deleteAppointmentInfo(e) {
+    if (e.target.classList.contains('delete-btn')) {
+      console.log(e.target.parentNode.parentNode); 
+      const rowElement = e.target.parentNode.parentNode;
+      const AppointmentId = rowElement.getAttribute('data-AppointmentId'); 
+      rowElement.remove(); 
+      fetch(baseUrl + `api/v1/Appointment/${AppointmentId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        console.log('Appointment Information deleted successfully');
+      })
+      .catch(error => {
+        console.error('There was an error deleting Appointment Information:', error);
+      });
+    }
+  }
+
+
+  function UpdateAppointmentInfo(e) {
+   
+    if (e.target.classList.contains('update-btn')) {
+      const rowElement = e.target.parentNode.parentNode;
+      const AppointmentId= rowElement.getAttribute('data-AppointmentId');
+      localStorage.setItem('AppointmentId', AppointmentId)
+      window.location.href = "Scheduling.html"
+      
+    } 
+  }
+  
+  
+  
+  window.addEventListener('load', updateAppointmentContent);
+
+  async function updateAppointmentContent(){
+      if(AppointmentId){
+          await fetch(baseUrl+`api/v1/appointment/${AppointmentId}`,{
+              method: 'GET',
+              
+          })
+          .then(res=>{
+              return res.json();
+          })
+          .then(data =>{
+              console.log(data);
+               document.getElementById('Appointment_Date').value = data.data.Appointment_Date
+               document.getElementById('Appointment_Time').value = data.data.Appointment_Time
+              document.getElementById("Patient_ID").value = data.data.Patient_ID
+            
+          })
+          .catch(error => console.log(error));
+      }
+  }
--- Front-end/Sub_Admin/Template/js/fetch.js
+++ Front-end/Sub_Admin/Template/js/fetch.js
@@ -1,41 +1,141 @@
-// Function to fetch health records using the Fetch API
-async function fetchHealthRecords() {
-    const endpoint = '/api/patients'; // Assuming this endpoint returns patient data
-    
-    try {
-      const response = await fetch(endpoint);
+const PatientId = localStorage.getItem('PatientId')
+
+const baseUrl = "http://localhost:7001/"
+async function fetchHealthRecords(e) {
+  if(e){
+    e.preventDefault();
+  }
+  if (PatientId) {
+    await fetch(baseUrl + `api/v1/patient/${PatientId}`, {
+        method: 'PATCH',
+        headers:{
+          Accept: "application/json",
+          "Content-Type": "application/json"
+      },
+        body: JSON.stringify({
+          patientName : document.getElementById('Patient_Name').value,
+          patientLocation : document.getElementById('Patient_Location').value,
+          patientPNumber : document.getElementById("Patient_Phone_Number").value
+        })
+    });
+    localStorage.removeItem("PatientId");
+} else {
+  const res = await fetch(baseUrl+"api/v1/patient",{
+    method: "POST",
+    headers:{
+        Accept: "application/json",
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      patientName : document.getElementById('Patient_Name').value,
+      patientLocation : document.getElementById('Patient_Location').value,
+      patientPNumber : document.getElementById("Patient_Phone_Number").value
+    })
   
+})
+}
+document.getElementById('patient-form').reset();
+}
+const regBtn = document.getElementById('btn-primary-submit');
+regBtn.addEventListener('click',fetchHealthRecords);
+
+
+async function retrievePatients(){
+  fetch(baseUrl + 'api/v1/patient')
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    console.log(data);
+    let containerDiv = document.querySelector('.patient-records');
+    for (let i = 0; i < data.data.length; i++) {
+      let html = `<tr data-patientId="${data.data[i].patientId}">
+                    <td>${data.data[i].patientId}</td>
+                    <td>${data.data[i].patientName}</td>
+                    <td>${data.data[i].patientLocation}</td>
+                    <td>${data.data[i].patientPNumber}</td>
+                    <td>987569326</td>
+                    <td>
+                      <i class="fas fa-edit action-btn update-btn"></i>
+                      <i class="fas fa-trash-alt action-btn delete-btn"></i>
+                    </td>
+                  </tr>`;
+      containerDiv.insertAdjacentHTML('beforeend', html);
+    }
+    const tableEl = document.querySelector('.tableEl');
+    tableEl.addEventListener('click',deletePatientInfo);
+    const updateEl = document.querySelector('.tableEl')
+    updateEl.addEventListener('click',UpdatePatientInfo);
+   
+  });
+}
+
+
+function deletePatientInfo(e) {
+  if (e.target.classList.contains('delete-btn')) {
+    console.log(e.target.parentNode.parentNode); 
+    const rowElement = e.target.parentNode.parentNode;
+    const patientId = rowElement.getAttribute('data-PatientId'); 
+    console.log(patientId)
+    rowElement.remove(); 
+    fetch(baseUrl + `api/v1/patient/${patientId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-  
-      const patientsData = await response.json();
-      return patientsData;
-    } catch (error) {
-      console.error('Error fetching health records:', error);
-      throw error;
-    }
-  }
-  
-  // Function to display fetched health records
-  function displayHealthRecords(records) {
-    const recordsList = document.getElementById('health-records-list');
-    recordsList.innerHTML = '';
-  
-    records.forEach(record => {
-      const recordItem = document.createElement('li');
-      recordItem.textContent = `Patient ID: ${record.patient_id}, Name: ${record.name}, Age: ${record.age}`;
-      recordsList.appendChild(recordItem);
+      console.log('Patient record deleted successfully');
+    })
+    .catch(error => {
+      console.error('There was an error deleting Patient record:', error);
     });
   }
-  
-  // Fetch and display health records when the page loads
-  window.addEventListener('load', async () => {
-    try {
-      const healthRecords = await fetchHealthRecords();
-      displayHealthRecords(Object.values(healthRecords)); // Convert object values to an array
-    } catch (error) {
-      console.error('Failed to fetch or display health records:', error);
+}
+
+function UpdatePatientInfo(e) {
+   
+  if (e.target.classList.contains('update-btn')) {
+    const rowElement = e.target.parentNode.parentNode;
+    const PatientId= rowElement.getAttribute('data-PatientId');
+    localStorage.setItem('PatientId', PatientId)
+    window.location.href = "patient_management.html"
+    
+  } 
+}
+
+
+
+window.addEventListener('load', updatePatientContent);
+async function updatePatientContent(){
+    if(PatientId){
+        await fetch(baseUrl+`api/v1/patient/${PatientId}`,{
+            method: 'GET',
+            
+        })
+        .then(res=>{
+            return res.json();
+        })
+        .then(data =>{
+            console.log(data);
+
+            document.getElementById('Patient_Name').value= data.data.patientName;
+            document.getElementById('Patient_Location').value= data.data.patientLocation;
+            document.getElementById('Patient_Phone_Number').value= data.data.patientPNumber;
+          
+        })
+        .catch(error => console.log(error));
     }
-  });
-  
+}
+
+
+// let popup = document.getElementById('popup')
+// function openPopup(){
+//     popup.classList.add('open-popup')
+// }
+// function closePopup(){
+//     popup.classList.remove('open-popup')
+// }
